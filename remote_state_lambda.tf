b2316ff7222b3874e113101225af366732f2ead5
ThiagoFrederico <thiagofrederico3@gmail.com>
2026-01-01 07:15:00 -0300

    remove

--- remote_state_lambda.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "lambda" {
-  backend = "s3"
-
-  config = {
-    bucket = "tf-state-challenge-bucket"
-    key    = "lambda/develop/terraform.tfstate"
-    region = "us-east-2"
-  }
-}
