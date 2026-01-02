6d91fe1746d281f4524eaa5ec51c40cb300dc8da
ThiagoFrederico <thiagofrederico3@gmail.com>
2026-01-02 02:19:36 -0300

    change output

--- outputs.tf
+++ outputs.tf
@@ -15,6 +15,11 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
+output "vpc_cidr" {
+  description = "CIDR da VPC (usado para regras de SG)"
+  value       = aws_vpc.main.cidr_block
+}
+
 output "subnet_ids" {
   description = "Subnets usadas pelo RDS"
   value = [
