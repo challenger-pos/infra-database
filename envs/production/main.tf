7ab4908faa25cff2dd6c556143aa5a9cb35f95e6
ThiagoFrederico <thiagofrederico3@gmail.com>
2026-01-09 16:32:19 -0300

    teste pipe

--- envs/production/main.tf
+++ envs/production/main.tf
@@ -44,9 +44,9 @@ module "rds" {
     module.security_groups.lambda_sg_id,
   ]
 
-  db_name     = "challengeone"
-  username    = "postgres"
-  password    = "postgres123"
+  db_name     = var.db_name
+  username    = var.db_user
+  password    = var.db_password
   environment = "production"
 }
 
--- envs/production/variables.tf
+++ envs/production/variables.tf
@@ -4,3 +4,17 @@ variable "region" {
 variable "environment" {
   type = string
 }
+
+variable "db_user" {
+  type      = string
+  sensitive = true # Recomendado para segredos
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_name" {
+  type = string
+}
