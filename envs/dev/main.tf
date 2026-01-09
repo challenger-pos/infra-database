1becd799149d3372e7ca90b63fa9b16f10ecf8e7
ThiagoFrederico <thiagofrederico3@gmail.com>
2026-01-09 17:21:39 -0300

    teste pipe

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -21,13 +21,13 @@ module "vpc" {
 
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
   private_azs     = ["us-east-2a", "us-east-2b"]
-  environment = "dev"
+  environment     = "dev"
 }
 
 module "security_groups" {
   source = "../../modules/security-groups"
 
-  vpc_id     = module.vpc.vpc_id
+  vpc_id      = module.vpc.vpc_id
   environment = "dev"
 }
 
